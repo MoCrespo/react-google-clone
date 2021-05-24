02bd8b7de6b3a1022e94005235c655f5f70d95a7
MoCrespo <mcrespo495@gmail.com>
2021-05-24 12:06:32 +0200

    import all things to app.js

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import './main.css';
+import Home from './pages/Home';
+import SearchPage from './pages/SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+
+
+      
     </div>
   );
 }
